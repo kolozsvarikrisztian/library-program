84a0d996b02031ee08d56a5f67741792c16c251f
Kolozsvári László Krisztián <laszlokrisztiankolozsvari@gmail.com>
2022-03-24 09:41:33 +0100

    kategória számlálás javítása

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
-    fantasyDb = 0;   
-    regenyDB = 0;
-    motivaciosDB = 0; 
+    szamlalok = {}
+    elemek = $('#kategoriak').find('input');
+    for (elem of elemek){
+        szamlalok[elem['id']] = 0;
+        
+    }
+
     if (localStorage.getItem("panel")=="closed") {
         $('#kategoria').addClass('rejtett');
         $('.rejtett').css('width','0');
@@ -31,27 +35,39 @@ $(document).ready(function(){
     $('#megse').click(function(){
         $(this).hide();
     });
-    $('#fantasy').click(function(){
-        if($('#fantasy').prop('checked')){
-            fantasyDb++;        
-            sessionStorage.setItem('fantasy',fantasyDb);
-            $('#fantasy_lbl').text("Fantasy (" + fantasyDb + ") ");        
-        }
-    });
+    // $('#fantasy').click(function(){
+    //     if($('#fantasy').prop('checked')){
+    //         fantasyDb++;        
+    //         sessionStorage.setItem('fantasy',fantasyDb);
+    //         $('#fantasy_lbl').text("Fantasy (" + fantasyDb + ") ");        
+    //     }
+    // });
 
-    $('#regeny').click(function(){
-        if($('#regeny').prop('checked')){
-            regenyDB++;        
-            sessionStorage.setItem('regeny',regenyDB);
-            $('#regeny_lbl').text("Regény (" + regenyDB + ") ");        
-        }
-    });
+    // $('#regeny').click(function(){
+    //     if($('#regeny').prop('checked')){
+    //         regenyDB++;        
+    //         sessionStorage.setItem('regeny',regenyDB);
+    //         $('#regeny_lbl').text("Regény (" + regenyDB + ") ");        
+    //     }
+    // });
 
-    $('#motivacios').click(function(){
-        if($('#motivacios').prop('checked')){
-            motivaciosDB++;        
-            sessionStorage.setItem('motivacios',motivaciosDB);
-            $('#motivacios_lbl').text("Motivációs (" + motivaciosDB + ") ");        
+    // $('#motivacios').click(function(){
+    //     if($('#motivacios').prop('checked')){
+    //         motivaciosDB++;        
+    //         sessionStorage.setItem('motivacios',motivaciosDB);
+    //         $('#motivacios_lbl').text("Motivációs (" + motivaciosDB + ") ");        
+    //     }
+    // });
+    $('#kategoriak').on('click', 'input', function(event){
+        // azonosito = $(event.target).prop('id');
+        if (event.target['checked']){
+            const azonosito = event.target['id'];
+            szamlalok[azonosito]++;
+            const cimke =  '#' + azonosito + '_lbl'
+            const kiirando = $(event.target).data('szoveg')
+            sessionStorage.setItem(azonosito,szamlalok[azonosito]);
+            $(cimke).text(`${kiirando} (${szamlalok[azonosito]})`);
         }
+        
     });
 });
